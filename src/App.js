7a17145af20e8658b2741d4a725fd3f81067a5a0
rashid <rashidsahil1@gmail.com>
2023-04-02 16:22:48 +0530

    designs commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import HeaderContainers from './Header';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import { useEffect } from 'react';
+
+
+function App() {
+  useEffect(()=>{
+    const url = 'http://projects.raisonminds.com/api/data.json';
+    fetch(url,{
+     method:"GET",
+     body:JSON.stringify(),
+      headers: {
+        "Authorization": "Bearer k_sudl4l70"
+           },
+    }).then((response)=>response.json()).then((data)=>{
+     console.log(data);
+    }).catch(e=>{
+     console.log(e);
+    })
+   },[])
+  return (
+    <>
+    <HeaderContainers/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './header.css';
+
+
+function HeaderContainers(props) {
+    return (
+        <div>
+            <div className='header-main'>
+        <div className='header-left'>
+         <h1>Wookie <br/>Movies</h1>   
+         </div>
+         <div className='header-right'>
+         <div className="row g-3">
+  <div className="col-auto">
+ <h1>M</h1>
+  </div>
+  <div className="col-auto">
+    <label htmlFor="inputPassword2" className="visually-hidden">Password</label>
+    <input type="search" className="form-control" />
+  </div>
+</div>
+         </div>
+         </div>
+         <hr></hr>
+        </div>
+    )
+}
+
+export default HeaderContainers;
